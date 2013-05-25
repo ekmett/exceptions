cd53f856a73a8b0233b990be6b04f42f64aed08f
Fedor Gogolev <knsd@knsd.net>
2013-05-25 21:57:55 +0400

    Test for `catchJust`

--- tests/Control/Monad/Catch/Tests.hs
+++ tests/Control/Monad/Catch/Tests.hs
@@ -1,11 +1,12 @@
 {-# LANGUAGE DeriveDataTypeable #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE ExistentialQuantification #-}
+{-# LANGUAGE NamedFieldPuns #-}
 
 module Control.Monad.Catch.Tests (tests) where
 
 import Prelude hiding (catch)
 
-import Control.Monad (void)
 import Data.Data (Data, Typeable)
 
 import Control.Monad.Trans.Identity (IdentityT(..))
@@ -13,7 +14,7 @@ import Control.Monad.Reader (ReaderT(..))
 import Test.Framework (Test, testGroup)
 import Test.Framework.Providers.QuickCheck2 (testProperty)
 import Test.QuickCheck (Property, once)
-import Test.QuickCheck.Monadic (monadic, run)
+import Test.QuickCheck.Monadic (monadic, run, assert)
 import Test.QuickCheck.Property (morallyDubiousIOProperty)
 import qualified Control.Monad.State.Lazy as LazyState
 import qualified Control.Monad.State.Strict as StrictState
@@ -22,40 +23,69 @@ import qualified Control.Monad.Writer.Strict as StrictWriter
 import qualified Control.Monad.RWS.Lazy as LazyRWS
 import qualified Control.Monad.RWS.Strict as StrictRWS
 
-import Control.Monad.Catch (Exception, MonadCatch(..), runCatch)
+import Control.Monad.Catch (Exception, MonadCatch(..), runCatch,
+                            catchJust)
 
-data TestException = TestException
-    deriving (Show, Data, Typeable)
+data TestException = TestException String
+    deriving (Show, Eq, Data, Typeable)
 
 instance Exception TestException
 
-testMonadCatch :: (Functor m, MonadCatch m) => (m Property -> Property) -> Property
-testMonadCatch f = monadic f $ do
+data MSpec = forall m. (MonadCatch m) => MSpec
+    { mspecName :: String
+    , mspecRunner :: (m Property -> Property)
+    }
+
+testMonadCatch :: MSpec -> Property
+testMonadCatch MSpec { mspecRunner } = monadic mspecRunner $
     run $ catch failure handler
   where
-    failure = do
-        void $ throwM TestException
-        error "testMonadCatch"
+    failure = throwM (TestException "foo") >> error "testMonadCatch"
     handler (_ :: TestException) = return ()
 
-tests :: Test
-tests = testGroup "Control.Monad.Catch.Tests"
-    [ mkMonadCatch "IO" morallyDubiousIOProperty
-    , mkMonadCatch "IdentityT IO" $ io . runIdentityT
-    , mkMonadCatch "LazyState.StateT IO" $ io . flip LazyState.evalStateT ()
-    , mkMonadCatch "StrictState.StateT IO" $ io . flip StrictState.evalStateT ()
-    , mkMonadCatch "ReaderT IO" $ io . flip runReaderT ()
-    , mkMonadCatch "LazyWriter.WriterT IO" $ io . fmap tfst . LazyWriter.runWriterT
-    , mkMonadCatch "StrictWriter.WriterT IO" $ io . fmap tfst . StrictWriter.runWriterT
-    , mkMonadCatch "LazyRWS.RWST IO" $ \m -> io $ fmap tfst $ LazyRWS.evalRWST m () ()
-    , mkMonadCatch "StrictRWS.RWST IO" $ \m -> io $ fmap tfst $ StrictRWS.evalRWST m () ()
+testCatchJust :: MSpec -> Property
+testCatchJust MSpec { mspecRunner } = monadic mspecRunner $ do
+    nice <- run $ catchJust testException posFailure posHandler
+    assert $ nice == ("pos", True)
+    bad <- run $ catch (catchJust testException negFailure posHandler) negHandler
+    assert $ bad == ("neg", True)
+  where
+    testException (TestException s) = if s == "pos" then Just True else Nothing
+    posHandler x = return ("pos", x)
+    negHandler (_ :: TestException) = return ("neg", True)
+    posFailure = throwM (TestException "pos") >> error "testCatchJust pos"
+    negFailure = throwM (TestException "neg") >> error "testCatchJust neg"
 
-    , mkMonadCatch "CatchT Indentity" $ fromRight . runCatch
+tests :: Test
+tests = testGroup "Control.Monad.Catch.Tests" $ concat
+    [ monadCatchTests
+    , catchJustTests
     ]
   where
-    tfst :: (Property, ()) -> Property
-    tfst = fst
+    mspecs =
+        [ MSpec "IO" morallyDubiousIOProperty
+        , MSpec "IdentityT IO" $ io . runIdentityT
+        , MSpec "LazyState.StateT IO" $ io . flip LazyState.evalStateT ()
+        , MSpec "StrictState.StateT IO" $ io . flip StrictState.evalStateT ()
+        , MSpec "ReaderT IO" $ io . flip runReaderT ()
+        , MSpec "LazyWriter.WriterT IO" $ io . fmap tfst . LazyWriter.runWriterT
+        , MSpec "StrictWriter.WriterT IO" $ io . fmap tfst . StrictWriter.runWriterT
+        , MSpec "LazyRWS.RWST IO" $ \m -> io $ fmap tfst $ LazyRWS.evalRWST m () ()
+        , MSpec "StrictRWS.RWST IO" $ \m -> io $ fmap tfst $ StrictRWS.evalRWST m () ()
+
+        , MSpec "CatchT Indentity" $ fromRight . runCatch
+        ]
+
+    tfst :: (Property, ()) -> Property = fst
     fromRight (Left _) = error "fromRight"
     fromRight (Right a) = a
     io = morallyDubiousIOProperty
-    mkMonadCatch name f = testProperty ("MonadCatch " ++ name) $ once $ testMonadCatch f
+
+    monadCatchTests = map mkMonadCatch mspecs
+    mkMonadCatch = mkTestType "MonadCatch" testMonadCatch
+
+    catchJustTests = map mkCatchJust mspecs
+    mkCatchJust = mkTestType "catchJust" testCatchJust
+
+    mkTestType name test = \spec ->
+        testProperty (name ++ " " ++ mspecName spec) $ once $ test spec
